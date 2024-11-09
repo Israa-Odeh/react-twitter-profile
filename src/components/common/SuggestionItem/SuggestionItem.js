825b7d5565906028683bc6a2aa890f099c1c7080
Israa <Israa.K.Odeh@hotmail.com>
2024-11-09 03:42:49 +0200

    Set up a folder for common components

---
+++ src/components/common/SuggestionItem/SuggestionItem.js
@@ -0,0 +1,56 @@
+import { PiSealCheckFill } from "react-icons/pi";
+import { IoPersonSharp } from "react-icons/io5";
+import "./SuggestionItem.css";
+
+const SuggestionItem = ({
+  imageUrl,
+  displayName,
+  username,
+  isVerified = false,
+  followedBy = "",
+}) => {
+  return (
+    <div className="suggestion-item">
+      <div className="suggestion-item__content">
+        <img
+          className="suggestion-item__image"
+          src={imageUrl}
+          width={32}
+          height={32}
+          alt={`${displayName}'s profile avatar`}
+        />
+
+        <div className="suggestion-item__info">
+          {followedBy && (
+            <div className="suggestion-item__followed-by">
+              <IoPersonSharp className="suggestion-item__followed-by-icon" />
+              {followedBy}
+            </div>
+          )}
+          <div className="suggestion-item__display-name">
+            <a
+              href={`/profile/${username}`}
+              className="suggestion-item__display-name-link"
+            >
+              {displayName}
+            </a>
+            {isVerified && (
+              <PiSealCheckFill
+                className="suggestion-item__verified-icon"
+                size={16}
+              />
+            )}
+          </div>
+
+          <span className="suggestion-item__username">@{username}</span>
+        </div>
+      </div>
+
+      <button className="suggestion-item__button" type="button">
+        Follow
+      </button>
+    </div>
+  );
+};
+
+export default SuggestionItem;
